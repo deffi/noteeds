272dd8cf7f47635ad809a5ba8c691073c5a2f39c
Martin Herrmann <martin@famherrmann.de>
2020-11-15 12:16:34 +0100

    Add type hint

--- noteeds/util/timing.py
+++ noteeds/util/timing.py
@@ -1,10 +1,11 @@
+from typing import Generator
 from contextlib import contextmanager
 
 from time import perf_counter
 
 
 @contextmanager
-def stopwatch(description):
+def stopwatch(description: str) -> Generator:
     start_time = perf_counter()
     yield
     end_time = perf_counter()
