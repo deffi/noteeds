1b2995dd40682b6838da502e6032f686681434c0
Martin Herrmann <martin@famherrmann.de>
2020-11-15 12:17:17 +0100

    Add ProgressReceiver

---
+++ noteeds/util/__init__.py
@@ -0,0 +1,1 @@
+from .progress_receiver import ProgressReceiver
---
+++ noteeds/util/progress_receiver.py
@@ -0,0 +1,6 @@
+class ProgressReceiver:
+    def range(self, minimum: float, maximum: float):
+        pass
+
+    def value(self, value: float):
+        pass
