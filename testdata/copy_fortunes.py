8c9f91922ecb8a814e22fa9a10a749bf837ea3f0
Martin Herrmann <martin@famherrmann.de>
2020-11-22 14:41:59 +0100

    Add script to copy fortune cookies as test data

---
+++ testdata/copy_fortunes.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+import shutil
+
+
+def main():
+    source = Path("/") / "usr" / "share" / "games" / "fortunes"
+    target = Path(__file__).parent / "fortunes"
+
+    target.mkdir(parents=True, exist_ok=True)
+    for source_file in source.glob("*"):
+        if "." not in source_file.name:
+            target_file = target / source_file.name
+            print(target_file)
+            shutil.copy(source_file, target_file)
+
+
+if __name__ == "__main__":
+    main()
--- testdata/readme.txt
+++ testdata/readme.txt
@@ -1,5 +1,7 @@
 Test data is not included in the repository for size and copyright reasons. Use
 the provided scripts to download test data:
+  * copy_fortunes - Fortune cookie files
+    Source: /usr/share/games/fortunes/
   * download_faust.py - Goethe's Faust
     Source: https://www.projekt-gutenberg.org/goethe/faust1/
   * download_jargon.py - The Jargon File (Jargon Lexicon only)
