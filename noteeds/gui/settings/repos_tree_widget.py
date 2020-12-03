147fe53f04216cd5da7813895278061f8f896509
Martin Herrmann <martin@famherrmann.de>
2020-12-03 22:54:11 +0100

    Factor out repos tree functionality from SettingsDialog

--- noteeds/gui/settings/repos_tree_widget.py
+++ noteeds/gui/settings/repos_tree_widget.py
@@ -1,22 +1,142 @@
-from PySide2.QtCore import Signal, Qt
+from typing import Optional, List
+
 from PySide2.QtGui import QKeyEvent
 from PySide2.QtWidgets import QTreeWidget
 from PySide2.QtGui import QDragMoveEvent
+from PySide2.QtCore import Qt
+from PySide2.QtGui import QColor
+from PySide2.QtWidgets import QWidget, QTreeWidgetItem, QAbstractItemView
+
+from noteeds.engine.repository import Config as RepoConfig
+from noteeds.gui.settings import ColorDelegate, PathBrowseDelegate
 
 
 class ReposTreeWidget(QTreeWidget):
-    delete_key = Signal()
-    insert_key = Signal()
+    def __init__(self, parent: Optional[QWidget]):
+        super().__init__(parent)
+
+        self._add_repo_item: Optional[QTreeWidgetItem] = None
+
+        # Settings
+        self.setEditTriggers(QAbstractItemView.AllEditTriggers & ~QAbstractItemView.CurrentChanged)
+
+        # Item delegates
+        self.setItemDelegateForColumn(1, ColorDelegate(self))
+        self.setItemDelegateForColumn(2, PathBrowseDelegate(self))
+
+        # Item signal
+        self.itemActivated.connect(self._item_activated)
+        self.itemClicked.connect(self._item_activated)
+
+    ##########
+    # Widget #
+    ##########
+
+    def resize_columns_to_contents(self):
+        for column in range(self.columnCount()):
+            self.resizeColumnToContents(column)
+
+    def selection_index(self):
+        selected_items = self.selectedItems()
+        if selected_items:
+            return self.indexOfTopLevelItem(selected_items[0])
+        else:
+            return 0
+
+    #########
+    # Items #
+    #########
+
+    @staticmethod
+    def _create_repo_item(repo: RepoConfig) -> QTreeWidgetItem:
+        item = QTreeWidgetItem()
+        item.setFlags(item.flags() & ~Qt.ItemIsDropEnabled | Qt.ItemIsEditable)
+
+        item.setData(0, Qt.CheckStateRole, (Qt.Checked if repo.enabled else Qt.Unchecked))
+        item.setText(0, repo.name or "")
+        item.setData(1, Qt.DisplayRole, "")
+        item.setData(1, Qt.DecorationRole, repo.color)
+        item.setData(2, Qt.EditRole, str(repo.root))
+
+        return item
+
+    @staticmethod
+    def _create_new_repo_item() -> QTreeWidgetItem:
+        item = QTreeWidgetItem()
+        item.setFlags(item.flags() | Qt.ItemIsEditable | Qt.ItemIsUserCheckable
+                      | Qt.ItemIsDragEnabled & ~Qt.ItemIsDropEnabled )
+
+        item.setData(0, Qt.CheckStateRole, Qt.Checked)
+        item.setText(0, "")
+        item.setData(1, Qt.DisplayRole, "")
+        item.setData(1, Qt.DecorationRole, None)
+        item.setData(2, Qt.EditRole, "")
+
+        return item
+
+    @staticmethod
+    def _create_add_item() -> QTreeWidgetItem:
+        item = QTreeWidgetItem()
+        item.setFlags(item.flags() & ~Qt.ItemIsEditable & ~Qt.ItemIsUserCheckable
+                      & ~Qt.ItemIsDragEnabled & ~Qt.ItemIsDropEnabled )
+
+        item.setData(0, Qt.DisplayRole, "Add...")
+        item.setData(0, Qt.TextColorRole, QColor (127, 127, 127))
+
+        return item
+
+    #########
+    # Repos #
+    #########
+
+    def set_repos(self, repos: List[RepoConfig]) -> None:
+        self.clear()
+        for repo in repos:
+            self.addTopLevelItem(self._create_repo_item(repo))
+
+        self._add_repo_item = self._create_add_item()
+        self.addTopLevelItem(self._add_repo_item)
+
+    def get_repos(self) -> List[RepoConfig]:
+        return []
+
+    def add_repo(self, position: int):
+        item = self._create_new_repo_item()
+        self.insertTopLevelItem(position, item)
+
+        self.clearSelection()
+        self.setCurrentItem(item)
+        item.setSelected(True)
+        self.editItem(item, 0)
+
+    def delete_selected_repos(self):
+        for item in self.selectedItems():
+            if item != self._add_repo_item:
+                self.invisibleRootItem().removeChild(item)
+
+    ####################
+    # User interaction #
+    ####################
 
     def keyPressEvent(self, event: QKeyEvent) -> None:
         if event.key() == Qt.Key_Delete:
-            self.delete_key.emit()
+            self.delete_selected_repos()
             event.accept()
         elif event.key() == Qt.Key_Insert:
-            self.insert_key.emit()
+            self.add_repo(self.selection_index())
             event.accept()
         else:
             super().keyPressEvent(event)
+
+    def _item_activated(self, item: QTreeWidgetItem, column: int):
+        # If the add-repo item was activated, add a new repository before that
+        # item.
+        if item == self._add_repo_item and column == 0:
+            self.add_repo(self.indexOfTopLevelItem(item))
+
+    #################
+    # Drag and drop #
+    #################
 
     def _drop_index(self, event: QDragMoveEvent) -> int:
         index = self.indexAt(event.pos())
@@ -31,7 +151,6 @@ class ReposTreeWidget(QTreeWidget):
             return index.row()
 
     def dragMoveEvent(self, event: QDragMoveEvent) -> None:
-        # TODO that does not belong in a generic tree widget, refactor to ReposTreeWidget
         # Update dropIndicatorPosition
         self.setDropIndicatorShown(True)
         super().dragMoveEvent(event)
--- noteeds/gui/settings/settings_dialog.py
+++ noteeds/gui/settings/settings_dialog.py
@@ -22,77 +22,10 @@ class SettingsDialog(QDialog):
         self.ui.hotkeyInput.editingFinished.connect(self.ui.hotkeyCheckbox.setFocus)
         self.ui.clearHotkeyButton.clicked.connect(self.ui.hotkeyInput.clear)
 
-        self.ui.reposTree.setItemDelegateForColumn(1, ColorDelegate(self))
-        self.ui.reposTree.setItemDelegateForColumn(2, PathBrowseDelegate(self))
-        self.ui.reposTree.setEditTriggers(QAbstractItemView.AllEditTriggers & ~QAbstractItemView.CurrentChanged)
-        self.ui.reposTree.insert_key.connect(self.repos_insert)
-        self.ui.reposTree.delete_key.connect(self.repos_delete)
-        self.ui.reposTree.itemActivated.connect(self.repos_item_activated)
-        self.ui.reposTree.itemClicked.connect(self.repos_item_activated)
-
     def set_config(self, config: Config):
         self.ui.systrayCheckbox.setChecked(config.gui.use_systray)
         self.ui.hotkeyCheckbox.setChecked(config.gui.use_global_hotkey)
         # self.ui.hotkeyInput.setText("+".join(config.gui.global_hotkey))
 
-        self.ui.reposTree.clear()
-        for repo in config.repositories:
-            item = QTreeWidgetItem()
-            item.setFlags(item.flags() & ~Qt.ItemIsDropEnabled | Qt.ItemIsEditable)
-            item.setData(0, Qt.CheckStateRole, (Qt.Checked if repo.enabled else Qt.Unchecked))
-            item.setText(0, repo.name or "")
-            item.setData(1, Qt.DisplayRole, "")
-            item.setData(1, Qt.DecorationRole, repo.color)
-            item.setData(2, Qt.EditRole, str(repo.root))
-
-            self.ui.reposTree.addTopLevelItem(item)
-
-        # The "Add..." item
-        item = QTreeWidgetItem()
-        item.setFlags(item.flags() & ~Qt.ItemIsDropEnabled & ~Qt.ItemIsEditable & ~Qt.ItemIsUserCheckable & ~Qt.ItemIsDragEnabled)
-        item.setData(0, Qt.DisplayRole, "Add...")
-        item.setData(0, Qt.TextColorRole, QColor (127, 127, 127))
-        self.ui.reposTree.addTopLevelItem(item)
-
-        for column in range(self.ui.reposTree.columnCount()):
-            self.ui.reposTree.resizeColumnToContents(column)
-
-    #########
-    # Repos #
-    #########
-
-    def insert_repo(self, position: int):
-        item = QTreeWidgetItem()
-        item.setFlags(item.flags() & ~Qt.ItemIsDropEnabled | Qt.ItemIsEditable)
-        item.setData(0, Qt.CheckStateRole, Qt.Checked)
-        item.setText(0, "")
-        item.setData(1, Qt.DisplayRole, "")
-        item.setData(1, Qt.DecorationRole, None)
-        item.setData(2, Qt.EditRole, "")
-
-        self.ui.reposTree.insertTopLevelItem(position, item)
-
-        self.ui.reposTree.clearSelection()
-        self.ui.reposTree.setCurrentItem(item)
-        item.setSelected(True)
-        self.ui.reposTree.editItem(item, 0)
-
-    def repos_insert(self):
-        selected_items = self.ui.reposTree.selectedItems()
-        if selected_items:
-            position = self.ui.reposTree.indexOfTopLevelItem(selected_items[0])
-        else:
-            position = 0
-
-        self.insert_repo(position)
-
-    def repos_delete(self):
-        for item in self.ui.reposTree.selectedItems():
-            index = self.ui.reposTree.indexOfTopLevelItem(item)
-            if index != self.ui.reposTree.invisibleRootItem().childCount() - 1:
-                self.ui.reposTree.invisibleRootItem().removeChild(item)
-
-    def repos_item_activated(self, item: QTreeWidgetItem, column: int):
-        index = self.ui.reposTree.indexOfTopLevelItem(item)
-        if index == self.ui.reposTree.invisibleRootItem().childCount() - 1 and column == 0:
-            self.insert_repo(index)
+        self.ui.reposTree.set_repos(config.repositories)
+        self.ui.reposTree.resize_columns_to_contents()
