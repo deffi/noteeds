94e14aec2ae1c038acc20712b4c72b809dcffd2d
Martin Herrmann <martin@famherrmann.de>
2020-12-06 18:25:31 +0100

    Improve MultiFormatter implementation

--- noteeds/util/multi_formatter.py
+++ noteeds/util/multi_formatter.py
@@ -1,21 +1,16 @@
-import string
+from noteeds.util import Formatter
 
 
-class MultiFormatter(string.Formatter):
+class MultiFormatter:
     def __init__(self, *args, **kwargs):
         super().__init__()
         self.args = args
         self.kwargs = kwargs
 
+        self._formatter = Formatter()
         self.unused = set(range(len(args))) |set(kwargs.keys())
 
-    # TODO violates Liskov, also the duplicate format/vformat is ridiculous. Compose a util.Formatter.
     def format(self, format_string):
-        return self.vformat(format_string)
-
-    def vformat(self, format_string):
-        return super().vformat(format_string, self.args, self.kwargs)
-
-    def check_unused_args(self, used_args, args, kwargs):
-        super().check_unused_args(used_args, args, kwargs)
-        self.unused -= used_args
+        result = self._formatter.vformat(format_string, self.args, self.kwargs)
+        self.unused &= self._formatter.unused
+        return result
